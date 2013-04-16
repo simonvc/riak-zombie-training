8d3b3a31305c3e45ea7d609bc7e6084d2342d4b6
Simon Vans-Colina <simonvc@basho.com>
2013-04-16 14:45:01 +0100

    it doesnt work

--- jmr.js
+++ jmr.js
@@ -4,8 +4,25 @@
     "source":"function(riakObject) {
        var j = Riak.mapValuesJson(riakObject);
 
-       var d = 4;
-       if (d < 5) {
+       var lat2 = 33.799745;
+       var lon2 = -84.327171;
+       var lat1 = j.lat;
+       var lon1 = j.longd;
+       
+        var R = 6371; 
+        var x1 = lat2 - lat1;
+        var dLat = ( x1 * Math.PI / 180);
+        var x2 = lon2 - lon1;
+        var dLon = ( x2 * Math.PI / 180);
+
+
+        var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+          Math.cos(lat1 * Math.PI / 180) * Math.cos(lat2 * Math.PI / 180) *
+          Math.sin(dLon / 2) * Math.sin(dLon / 2);
+        var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        var d = R * c;
+
+       if (d < 5000) {
          return [[riakObject.key]];
        }
        else {
